a9d039aa257f769aaa3f27d270bf40e29cc34f70
falenag@gmail.com <falenag@gmail.com>
2023-06-17 19:30:20 +0900

    Добавьте файлы проекта.

---
+++ Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputD = "Введите  число";
+            Console.WriteLine(InputD);
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(a % 2 == 0 ? "Четное" : "Не четное");
+
+            Console.WriteLine("");
+        }
+    }
+}
